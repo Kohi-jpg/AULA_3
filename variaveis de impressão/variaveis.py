01044f56d6080f6defe431dc60e03808a3e05e61
Luiz Felipe <52085357+Kohi-jpg@users.noreply.github.com>
2023-09-30 17:27:45 -0300

    Create variaveis.py

---
+++ variaveis de impressão/variaveis.py
@@ -0,0 +1,7 @@
+var1 = 123
+var2 = 'world'
+
+print("Hello to the {} {}", var1, var2)
+print("Hello to the {} {}" .format(var1 ,var2))
+print("Hello to the {} {}"+ var2 + str(var1))
+print(f"Hello to the {var1} {var2}")
