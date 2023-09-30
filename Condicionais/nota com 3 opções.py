2b604f8debdbf4f8a25bcbde84015043365b8185
Luiz Felipe <52085357+Kohi-jpg@users.noreply.github.com>
2023-09-30 17:31:30 -0300

    Create nota com 3 opções.py

---
+++ Condicionais/nota com 3 opções.py
@@ -0,0 +1,8 @@
+nota = float(input('Digite a nota:\n'))
+
+if nota == 10:
+    print('Você passou de ano')
+elif nota >= 7:
+    print('Você foi bem')
+elif nota <= 7:
+    print('Precisa melhorar')
