12692b5fe4098716a7144d336c854c3b5d877482
Luiz Felipe <52085357+Kohi-jpg@users.noreply.github.com>
2023-09-30 17:11:11 -0300

    Delete Condicionais.Exercicio 1 - comparação.py

--- Condicionais.Exercicio 1 - comparação.py
+++
@@ -1,7 +0,0 @@
-idade1 = int(input('Digite a idade:\n'))
-idade2 = int(input('Digite a segunda idade:\n'))
-
-if idade1 >= idade2:
-    print(f'{idade1} é maior que {idade2}')
-else:
-    print(f'{idade1} é menor que {idade2}')
