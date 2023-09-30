b00eba63e62d99efd4e356d8fe95f0d680bcd283
Luiz Felipe <52085357+Kohi-jpg@users.noreply.github.com>
2023-09-30 17:09:49 -0300

    Update and rename Condicional.votação.py to Condicionais/votação.py

--- Condicionais/votação.py
+++ Condicionais/votação.py
@@ -4,10 +4,3 @@ if age >= 18 and age <=65:
     print('Você já esta na idade para votar')
 else:
     print('Você não pode votar')
-
-    language = 'english'
-
-if not language == 'portuguese':
-    print('contratado')
-else:
-    print('não contratado')
